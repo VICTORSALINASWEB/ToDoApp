b3837fead18607af12e3beaf806c60be98469b28
victor <victorsalinasweb@gmail.com>
2026-07-25 07:46:07 -0500

    20260725_vs_mostrar alerta de seccion agotada

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,12 +1,47 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular/standalone';
 import { LocalStorageService } from '../services/local-storage.service';
-export const authGuard: CanActivateFn = () => {
-  const router = inject(Router);
-  const localStorageService = inject(LocalStorageService);
+import { StorageService } from '../services/storage.service';
 
-  const token = localStorageService.obtenerToken(); 
-  if (token) return true; 
-  router.navigateByUrl('/login');
-  return false;
+export const authGuard: CanActivateFn = async () => {
+  const localStorageService = inject(LocalStorageService);
+  const storageService = inject(StorageService);
+  const alertCtrl = inject(AlertController);
+  const router = inject(Router);
+
+  const token = localStorageService.obtenerToken();
+
+  if (!token) {
+    router.navigateByUrl('/login');
+    return false;
+  }
+
+
+  const expiraEnMs = localStorageService.obtenerExpiracion(); 
+  
+  if ((expiraEnMs && Date.now() > expiraEnMs) || expiraEnMs === null) {
+    const alert = await alertCtrl.create({
+      header: 'Sesión expirada',
+      message: 'Tu tiempo de sesión ha terminado. Por favor, inicia sesión nuevamente.',
+      backdropDismiss: false,
+      buttons: [
+        {
+          text: 'OK',
+          handler: async () => {
+            await localStorageService.limpiartodo();
+            await storageService.limpiartodo();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+    await alert.onDidDismiss();
+
+    router.navigateByUrl('/login');
+    return false;
+  }
+
+  return true;
 };
