5aa73ea35827212fb4df49fe0978539d52d3d3bb
victor <victorsalinasweb@gmail.com>
2026-07-25 07:57:38 -0500

    20260725_vs_conexion a internet

--- src/app/core/services/network.service.ts
+++ src/app/core/services/network.service.ts
@@ -1,21 +1,35 @@
 // network.service.ts
 import { Injectable, signal } from '@angular/core';
 import { Network } from '@capacitor/network';
+import { UiUtilService } from './ui-util.service';
 
 @Injectable({ providedIn: 'root' })
 export class NetworkService {
   isOnline = signal<boolean>(true);
 
-  constructor() {
+  constructor(
+    private uiUtilService: UiUtilService
+  ) {
     this.init();
   }
 
-  private async init() {
-    const status = await Network.getStatus();
+ private async init() {
+  const status = await Network.getStatus();
+  this.isOnline.set(status.connected);
+
+  let ultimoEstado = status.connected;
+
+  Network.addListener('networkStatusChange', (status) => {
+    if (status.connected === ultimoEstado) return; // evita toasts duplicados
+    ultimoEstado = status.connected;
+
     this.isOnline.set(status.connected);
 
-    Network.addListener('networkStatusChange', (status) => {
-      this.isOnline.set(status.connected);
-    });
-  }
+    if (this.isOnline()) {
+      this.uiUtilService.toastExito('Tiene conexión a internet');
+    } else {
+      this.uiUtilService.toastError('No tiene conexión a internet');
+    }
+  });
+}
 }
